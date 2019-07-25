69f0ee23cff50972e274608cf8908bca3abbdd02
Rahul Akarapu <rahul.akarapu@sap.com>
2019-07-25 21:34:25 +0530

    Updates

---
+++ precache-manifest.8edde9f8e3bfc164814f01afa5052b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1daf0213835f8ff4dd2ebae9423d00",
+    "url": "/theholykitchen/index.html"
+  },
+  {
+    "revision": "af37d69ae6fa218a130a",
+    "url": "/theholykitchen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9b770b6b26ace01d0431",
+    "url": "/theholykitchen/static/css/main.cbedd7f2.chunk.css"
+  },
+  {
+    "revision": "af37d69ae6fa218a130a",
+    "url": "/theholykitchen/static/js/2.27e95713.chunk.js"
+  },
+  {
+    "revision": "9b770b6b26ace01d0431",
+    "url": "/theholykitchen/static/js/main.893a82e2.chunk.js"
+  },
+  {
+    "revision": "760a68c9d1efe9ba453d",
+    "url": "/theholykitchen/static/js/runtime~main.01b60668.js"
+  },
+  {
+    "revision": "56dfe7cdeeb9445ec506c382d7df1736",
+    "url": "/theholykitchen/static/media/RestaurantBG1.56dfe7cd.jpg"
+  }
+]);
